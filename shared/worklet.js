60c93b50e538089615fb012f260505f66f96f0d3
Kasper Isager Dalsgarð <kasperisager@hey.com>
2024-07-01 13:08:19 +0200

    Add `BareKit` global

--- shared/worklet.js
+++ shared/worklet.js
@@ -1,8 +1,24 @@
-/* global Bare */
 const IPC = require('bare-ipc')
 
 const ports = IPC.open()
 
-Bare.IPC = new IPC(ports[0])
+class BareKit {
+  constructor () {
+    this.IPC = new IPC(ports[0])
+  }
+
+  [Symbol.for('bare.inspect')] () {
+    return {
+      __proto__: { constructor: BareKit }
+    }
+  }
+}
+
+Object.defineProperty(global, 'BareKit', {
+  value: new BareKit(),
+  enumerable: true,
+  writable: false,
+  configurable: true
+})
 
 module.exports = ports[1]
