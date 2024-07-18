0e2d821ddd5ca905826dfe2dba52a97820cd1cef
Kasper Isager Dalsgarð <kasperisager@hey.com>
2024-07-18 11:23:20 +0200

    Add Android push API

--- android/src/main/java/to/holepunch/bare/kit/Worklet.java
+++ android/src/main/java/to/holepunch/bare/kit/Worklet.java
@@ -1,5 +1,7 @@
 package to.holepunch.bare.kit;
 
+import android.os.Handler;
+import android.os.Looper;
 import java.io.Closeable;
 import java.io.FileDescriptor;
 import java.io.IOException;
@@ -7,14 +9,30 @@ import java.io.InputStream;
 import java.nio.ByteBuffer;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
+import java.nio.charset.Charset;
 
 public class Worklet implements Closeable {
+  @FunctionalInterface
+  public interface PushCallback<T> {
+    void
+    apply (T reply, Throwable exception);
+  }
+
+  @FunctionalInterface
+  private interface NativePushCallback {
+    void
+    apply (ByteBuffer reply, String exception);
+  }
+
   private ByteBuffer handle;
   private FileDescriptor incoming;
   private FileDescriptor outgoing;
+  private Handler handler;
 
   public Worklet() {
     handle = init();
+
+    handler = Handler.createAsync(Looper.getMainLooper());
   }
 
   private native ByteBuffer
@@ -38,6 +56,9 @@ public class Worklet implements Closeable {
   private native FileDescriptor
   outgoing (ByteBuffer handle);
 
+  private native void
+  push (ByteBuffer handle, ByteBuffer payload, int len, NativePushCallback callback);
+
   private void
   start (String filename, ByteBuffer source, int len) {
     start(handle, filename, source, len);
@@ -48,9 +69,15 @@ public class Worklet implements Closeable {
 
   public void
   start (String filename, ByteBuffer source) {
-    ByteBuffer buffer = ByteBuffer.allocateDirect(source.limit());
-    buffer.put(source);
-    buffer.flip();
+    ByteBuffer buffer;
+
+    if (source.isDirect()) {
+      buffer = source;
+    } else {
+      buffer = ByteBuffer.allocateDirect(source.limit());
+      buffer.put(source);
+      buffer.flip();
+    }
 
     start(filename, buffer, buffer.limit());
   }
@@ -110,6 +137,49 @@ public class Worklet implements Closeable {
   public FileDescriptor
   outgoing () {
     return outgoing;
+  }
+
+  private void
+  push (ByteBuffer payload, int len, PushCallback<ByteBuffer> callback) {
+    push(handle, payload, len, (reply, error) -> {
+      ByteBuffer buffer = reply == null ? null : ByteBuffer.allocateDirect(reply.limit());
+
+      if (buffer != null) {
+        buffer.put(reply);
+        buffer.flip();
+      }
+
+      Throwable exception = error == null ? null : new Error(error);
+
+      handler.post(() -> callback.apply(buffer, exception));
+    });
+  }
+
+  public void
+  push (ByteBuffer payload, PushCallback<ByteBuffer> callback) {
+    ByteBuffer buffer;
+
+    if (payload.isDirect()) {
+      buffer = payload;
+    } else {
+      buffer = ByteBuffer.allocateDirect(payload.limit());
+      buffer.put(payload);
+      buffer.flip();
+    }
+
+    push(buffer, buffer.limit(), callback);
+  }
+
+  public void
+  push (String payload, Charset charset, PushCallback<String> callback) {
+    push(ByteBuffer.wrap(payload.getBytes(charset)), (reply, error) -> {
+      callback.apply(reply == null ? null : charset.decode(reply).toString(), error);
+    });
+  }
+
+  public void
+  push (String payload, String charset, PushCallback<String> callback) {
+    push(payload, Charset.forName(charset), callback);
   }
 
   public void
