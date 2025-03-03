83cafe8d3ee327fdfd989fb73f6d6c8e38238a0e
Tony Gorez <gorez.tony@gmail.com>
2025-03-03 14:53:33 +0100

    test(ios): add basic test app
    Signed-off-by: Tony Gorez <gorez.tony@gmail.com>

---
+++ test/ios/worklet.m
@@ -0,0 +1,44 @@
+#import <BareKit/BareKit.h>
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@property (strong, nonatomic) UIWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    UIViewController *rootViewController = [[UIViewController alloc] init];
+    self.window.rootViewController = rootViewController;
+    [self.window makeKeyAndVisible];
+
+    // Run the worklet test
+    BareWorklet *worklet = [[BareWorklet alloc] initWithConfiguration:nil];
+    NSString *source = @"BareKit.IPC.on('data', (data) => BareKit.IPC.write(data)).write('Hello!')";
+
+    [worklet start:@"/app.js" source:[source dataUsingEncoding:NSUTF8StringEncoding] arguments:@[]];
+
+    BareIPC *ipc = [[BareIPC alloc] initWithWorklet:worklet];
+
+    // Rest of the IPC test code remains the same
+    ipc.readable = ^(BareIPC *ipc) {
+        NSString *data = [ipc read:NSUTF8StringEncoding];
+        if (data == nil) return;
+
+        ipc.readable = nil;
+        NSLog(@"%@", data);
+    };
+
+    return YES;
+}
+
+@end
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+} 
+
