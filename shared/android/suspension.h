8d9674d744f4ca7cb007708c28f3a2ff2336a2aa
Kasper Isager Dalsgarð <kasperisager@hey.com>
2025-07-03 13:59:43 +0200

    Add missing Android header

---
+++ shared/android/suspension.h
@@ -0,0 +1,16 @@
+#ifndef BARE_KIT_ANDROID_SUSPENSION_H
+#define BARE_KIT_ANDROID_SUSPENSION_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "../suspension.h"
+
+struct bare_suspension_s {};
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // BARE_KIT_ANDROID_SUSPENSION_H
