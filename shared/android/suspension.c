9b1fc884d40e57fa90c7f0d8227917588c72d23d
Kasper Isager Dalsgarð <kasperisager@hey.com>
2025-07-03 14:10:18 +0200

    Fix Android suspension stubs

--- shared/android/suspension.c
+++ shared/android/suspension.c
@@ -1,7 +1,12 @@
 #include "../suspension.h"
 
 int
-bare_suspension_start(int linger, bare_suspension_t **result) {
+bare_suspension_init(bare_suspension_t *suspension) {
+  return 0;
+}
+
+int
+bare_suspension_start(bare_suspension_t *result, int linger) {
   return linger < 0 ? 0 : linger;
 }
 
