2e6d9e58ad87a2cc942871382cf8c6410dbb3d72
Tomas Ravinskas <tomas@tomasrav.me>
2025-11-02 10:47:22 +0200

    Downgrade to v1.11.1

---
+++ shared/linux/ipc.h
@@ -0,0 +1,32 @@
+#ifndef BARE_KIT_LINUX_IPC_H
+#define BARE_KIT_LINUX_IPC_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <pthread.h>
+#include <stdbool.h>
+#include <sys/epoll.h>
+
+#include "../ipc.h"
+
+struct bare_ipc_poll_s {
+  bare_ipc_t *ipc;
+
+  int epoll_fd;
+  int close_fd;
+  pthread_t thread;
+
+  int events;
+
+  bare_ipc_poll_cb cb;
+
+  void *data;
+};
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // BARE_KIT_LINUX_IPC_H
